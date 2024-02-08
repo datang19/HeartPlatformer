8c2ced896f4ec7ffd1071ec73d11236ed014dee1
Jasmin <jahernandez16@s.sfusd.edu>
2024-02-08 15:40:39 -0800

    Copied files over

--- world.gd
+++ world.gd
@@ -5,4 +5,4 @@ extends Node2D
 
 func _ready():
 	RenderingServer.set_default_clear_color(Color.BLACK)
-	polygon_2d.polygon = collision_polygon_2d.polygon 
+	polygon_2d.polygon = collision_polygon_2d.polygon
